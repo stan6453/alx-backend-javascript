877c9af841570c00ace530c6dbc590d9b7eb72a6
stanley <stanley6453@gmail.com>
2023-06-26 13:54:58 +0100

    completed task 13 (100-createIteratorObject.js)

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,34 @@
 export default function createIteratorObject(report) {
+  const departments = Object.keys(report.allEmployees);
+  const departmentLength = departments.length;
+  let currentDepartment = 0;
 
+  let employees = report.allEmployees[departments[currentDepartment]];
+  let employeesLength = employees.length;
+  let currentEmployee = -1;
+
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      currentEmployee += 1;
+
+      if (currentEmployee === employeesLength || employeesLength === 0) {
+        currentDepartment += 1;
+        if (currentDepartment >= departmentLength) {
+          return { done: true };
+        }
+
+        employees = report.allEmployees[departments[currentDepartment]];
+        employeesLength = employees.length;
+        currentEmployee = 0;
+      }
+
+      return {
+        value: report.allEmployees[departments[currentDepartment]][currentEmployee],
+        done: false,
+      };
+    },
+  };
 }
-
