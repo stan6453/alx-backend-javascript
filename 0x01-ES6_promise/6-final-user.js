1e029b784ba22a5a5a0fa0f5ebad2042036379f5
stanley <stanley6453@gmail.com>
2023-06-27 12:31:57 +0100

    modified file 0x01-ES6_promise/6-final-user.js to conform to eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(), uploadPhoto()])
-    .then((result) => {
-      return result;
-    })
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => result);
 }
