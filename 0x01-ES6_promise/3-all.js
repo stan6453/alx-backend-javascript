656f2594249fac4668a36bbbac56e8761b6a93f6
stanley <stanley6453@gmail.com>
2023-06-27 09:15:23 +0100

    added file 0x01-ES6_promise/5-photo-reject.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { createUser, uploadPhoto } from './utils'
+
+export default function handleProfileSignup() {
+  Promise.all([createUser(), uploadPhoto()])
+    .then((result) => {
+      console.log(`${result[1].body} ${result[0].firstName} ${result[0].lastName}`)
+    })
+}
