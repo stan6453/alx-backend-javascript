d4f912b5dea4371f330987c267186c4972d72341
stanley <stanley6453@gmail.com>
2023-06-27 12:29:03 +0100

    modified file 0x01-ES6_promise/3-all.js to conform to eslint

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,3 @@
-export default function uploadPhoto(filename) {
-  return Promise.reject(`${filename} cannot be processed`)
+export default function uploadPhoto(fileName) {
+  return Promise.reject(`${fileName} cannot be processed`);
 }
