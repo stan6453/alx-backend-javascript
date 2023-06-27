bb124986e3724db62a384ecce1bf1f2dabdbbf5d
stanley <stanley6453@gmail.com>
2023-06-27 12:26:34 +0100

    modified file 0x01-ES6_promise/3-all.js to conform to eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,8 @@ export default function handleProfileSignup() {
   Promise.all([createUser(), uploadPhoto()])
     .then((result) => {
       console.log(`${result[1].body} ${result[0].firstName} ${result[0].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline ');
     });
 }
