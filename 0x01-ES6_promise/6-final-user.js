53fa70109b56e9ad670550c0a6bba8861c73ba98
stanley <stanley6453@gmail.com>
2023-06-27 19:59:38 +0100

    modified file 0x01-ES6_promise/{8-try.js, 9-try.js, 100-await.js} to conform to eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,20 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpUserPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then(
-    (values) => {
-      const result = [];
-      values.forEach((element) => {
-        if (element.status === 'fulfilled') {
-          result.push({ status: element.status, value: element.value });
-        } else {
-          result.push({ status: element.status, value: `${element.reason}` });
-        }
-      });
-      return result;
-    },
-  );
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ])
+    .then((result) => {
+      const newResult = [];
+      for (const obj of result) {
+        newResult.push({
+          status: obj.status,
+          value: obj.value === undefined ? `${obj.reason}` : obj.value,
+        });
+      }
+      return newResult;
+    });
 }
