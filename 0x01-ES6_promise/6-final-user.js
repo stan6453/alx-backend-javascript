244dec17b63c002170ff8121c5ac57be563193ca
stanley <stanley6453@gmail.com>
2023-06-27 13:22:25 +0100

    modified file 0x01-ES6_promise/6-final-user,js to return the right output

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
     .then((result) => {
       const newResult = [];
       for (const obj of result) {
-        newResult.push({ status: obj.status, value: obj.value === undefined ? obj.reason : obj.value });
+        newResult.push({
+          status: obj.status,
+          value: obj.value === undefined ? obj.reason : obj.value,
+        });
       }
       return newResult;
     });
