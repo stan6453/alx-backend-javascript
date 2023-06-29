9fbc3eaac6fd77d0f28bafb0a931eefa7b773835
stanley <stanley6453@gmail.com>
2023-06-29 18:18:21 +0100

    added file 0x02-ES6_classes/7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,4 +1,4 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
@@ -12,9 +12,7 @@ class Airport {
     return this._code;
   }
 
-  toString() {
-    return `[object ${this._code}]`;
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
   }
 }
-
-export default Airport;
