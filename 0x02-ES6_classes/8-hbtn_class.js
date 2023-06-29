97b32b3ad23b593e5c009481ec5597371383fedf
stanley <stanley6453@gmail.com>
2023-06-29 19:10:07 +0100

    added file 0x02-ES6_classes/8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,24 +1,13 @@
-class HolbertonClass {
+export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
   }
 
-  get size() {
-    return this._size;
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  valueOf() {
-    return this._size;
-  }
-
-  toString() {
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
     return this._location;
   }
 }
-
-export default HolbertonClass;
