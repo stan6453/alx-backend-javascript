0586ca5ee327cb1436d285be919579dcbb2c39d3
stanley <stanley6453@gmail.com>
2023-07-04 07:33:45 +0100

    modified file 0x03-ES6_data_manipulation/8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,3 @@
 export default function cleanSet(set, startString) {
-  return ([...set].reduce((sum, val) => sum + (val.startsWith(startString) && startString !== '' ? `-${val}` : ''), '')).slice(1);
+  return ([...set].reduce((sum, val) => sum + (val.startsWith(startString) && startString !== '' ? `-${val.slice(startString.length)}` : ''), '')).slice(1);
 }
