0d0bf0559c1154c4629c7ac1af57666286a4a157
stanley <stanley6453@gmail.com>
2023-07-04 07:31:00 +0100

    modified file 0x03-ES6_data_manipulation/5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,5 +2,5 @@ export default function createInt8TypedArray(length, position, value) {
   if (position > length - 1) throw Error('Position outside range');
   const binary8Array = new Uint8Array(length);
   binary8Array[position] = value;
-  return binary8Array.buffer;
+  return new DataView(binary8Array.buffer);
 }
