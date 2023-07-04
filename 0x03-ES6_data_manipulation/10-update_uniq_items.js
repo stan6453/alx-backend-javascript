9d333a67c74535331392a2f1b4c7d1f23f67f90d
stanley <stanley6453@gmail.com>
2023-07-04 07:48:15 +0100

    modified file 0x03-ES6_data_manipulation/10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,4 @@
 export default function updateUniqueItems(groceriesList) {
-  if (!groceriesList instanceof Map) throw new Error('Cannot process');
-  groceriesList.forEach((value, key) => value === 1 ? groceriesList.set(key, 100) : '');
+  if (!(groceriesList instanceof Map)) throw new Error('Cannot process');
+  groceriesList.forEach((value, key) => (value === 1 ? groceriesList.set(key, 100) : ''));
 }
