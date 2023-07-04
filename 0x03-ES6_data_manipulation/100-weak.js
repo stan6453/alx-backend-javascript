98b1526c125cd1cc38b25aa667cb574b5bf24eb0
stanley <stanley6453@gmail.com>
2023-07-04 08:04:07 +0100

    modified file 0x03-ES6_data_manipulation/100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     if (weakMap.get(endpoint) >= 5) {
-      throw new Error('Endpoint load is high');
+      throw Error('Endpoint load is high');
     }
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   } else {
