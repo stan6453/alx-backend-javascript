48a1f3c4ba43f24747723a8735b9cdfc7c8374e5
stanley <stanley6453@gmail.com>
2023-07-04 13:07:41 +0100

    modified file 0x03-ES6_data_manipulation/100-weak.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set) || typeof startString !== 'string') return '';
+  if (!(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) return '';
+
   const arr = [];
   for (const item of set) {
     if (item.startsWith(startString)) {
