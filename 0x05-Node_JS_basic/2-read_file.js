3e0b23ddfd2c6e656bac1638e5506da9f1ec62fc
stanley <stanley6453@gmail.com>
2023-08-21 19:15:30 +0100

    craete a server in node.js and handled reading and manipulating file content

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,32 +1,34 @@
 const fs = require('fs');
 
-function countStudents(file_path) {
-  let data
-  const study = {}
+function countStudents(filePath) {
+  let data;
+  const study = {};
   try {
-    data = fs.readFileSync(file_path, 'utf8');
+    data = fs.readFileSync(filePath, 'utf8');
   } catch (err) {
-    throw new Error('Cannot load the database')
+    throw new Error('Cannot load the database');
   }
 
-  data = data.split('\n')
-  data = data.slice(1)
-  data = data.filter(arr => arr.length > 1)
+  data = data.split('\n');
+  data = data.slice(1);
+  data = data.filter((arr) => arr.length > 1);
 
-  console.log(`Number of students: ${data.length}`)
+  console.log(`Number of students: ${data.length}`);
 
-  for (let i = 0; i < data.length; i++) {
-    row = data[i];
-    column = row.split(',')
+  for (let i = 0; i < data.length; i += 1) {
+    const row = data[i];
+    const column = row.split(',');
     if (!study[column[3]]) {
-      study[column[3]] = [column[0]]
+      study[column[3]] = [column[0]];
     } else {
-      study[column[3]].push(column[0])
+      study[column[3]].push(column[0]);
     }
   }
-  for (course in study) {
-    console.log(`Number of students in CS: ${study[course].length}. List: ${study[course].join(', ')}`);
+  for (const course in study) {
+    if (course) {
+      console.log(`Number of students in CS: ${study[course].length}. List: ${study[course].join(', ')}`);
+    }
   }
 }
 
-module.exports = countStudents
+module.exports = countStudents;
