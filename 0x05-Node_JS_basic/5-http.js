36b63ccb7c23c406a54440f8c4f29534a686df84
stanley <stanley6453@gmail.com>
2023-08-21 19:44:57 +0100

    craete a server in node.js and handled reading and manipulating file content

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,16 +1,50 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  }
+
+  if (req.url === '/students') {
+    fs.readFile('database.csv', 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      let response = 'This is the list of our students';
+      const study = {};
+      let tempData = data.split('\n');
+      tempData = tempData.slice(1);
+      tempData = tempData.filter((arr) => arr.length > 1);
+
+      response += (`Number of students: ${tempData.length}\n`);
+      for (let i = 0; i < tempData.length; i += 1) {
+        const row = tempData[i];
+        const column = row.split(',');
+        if (!study[column[3]]) {
+          study[column[3]] = [column[0]];
+        } else {
+          study[column[3]].push(column[0]);
+        }
+      }
+      for (const course in study) {
+        if (course) {
+          response += (`Number of students in ${course}: ${study[course].length}. List: ${study[course].join(', ')}\n`);
+        }
+      }
+      res.end(response.slice(0, -1));
+      return undefined;
+    });
+  }
 });
 
 app.listen(port, hostname, () => {
   console.log(`app running at http://${hostname}:${port}/`);
 });
 
-module.exports = app
+module.exports = app;
