e424cac91dde190728f66e51f26ce31bd47b3194
stanley <stanley6453@gmail.com>
2023-08-21 20:02:40 +0100

    craete a server in node.js and handled reading and manipulating file content

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,6 +5,39 @@ const hostname = '127.0.0.1';
 const port = 1245;
 const databaseName = process.argv[2];
 
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        return reject(new Error('Cannot load the database'));
+      }
+      let response = 'This is the list of our students\n';
+      const study = {};
+      let tempData = data.split('\n');
+      tempData = tempData.slice(1);
+      tempData = tempData.filter((arr) => arr.length > 1);
+
+      response += (`Number of students: ${tempData.length}\n`);
+      for (let i = 0; i < tempData.length; i += 1) {
+        const row = tempData[i];
+        const column = row.split(',');
+        if (!study[column[3]]) {
+          study[column[3]] = [column[0]];
+        } else {
+          study[column[3]].push(column[0]);
+        }
+      }
+      for (const course in study) {
+        if (course) {
+          response += (`Number of students in ${course}: ${study[course].length}. List: ${study[course].join(', ')}\n`);
+        }
+      }
+      resolve(response.slice(0, -1));
+      return undefined;
+    });
+  });
+}
+
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.statusCode = 200;
@@ -13,42 +46,13 @@ const app = http.createServer((req, res) => {
   }
 
   if (req.url === '/students') {
-    return new Promise((resolve) => {
-      fs.readFile(databaseName, 'utf-8', (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        let response = 'This is the list of our students';
-        const study = {};
-        let tempData = data.split('\n');
-        tempData = tempData.slice(1);
-        tempData = tempData.filter((arr) => arr.length > 1);
-
-        response += (`Number of students: ${tempData.length}\n`);
-        for (let i = 0; i < tempData.length; i += 1) {
-          const row = tempData[i];
-          const column = row.split(',');
-          if (!study[column[3]]) {
-            study[column[3]] = [column[0]];
-          } else {
-            study[column[3]].push(column[0]);
-          }
-        }
-        for (const course in study) {
-          if (course) {
-            response += (`Number of students in ${course}: ${study[course].length}. List: ${study[course].join(', ')}\n`);
-          }
-        }
+    countStudents(databaseName)
+      .then((result) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.end(response.slice(0, -1));
-        resolve('done');
-        return undefined;
+        res.end(result);
       });
-      return undefined;
-    });
   }
-  return undefined;
 });
 
 app.listen(port, hostname, () => {
