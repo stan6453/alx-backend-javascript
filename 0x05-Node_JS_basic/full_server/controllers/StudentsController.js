9cfd991060c07e867913ae141f31e477359e0cfd
stanley <stanley6453@gmail.com>
2023-08-22 12:48:18 +0100

    craete a server in node.js and handled reading and manipulating file content

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,6 +4,7 @@ export default class StudentsController {
   static getAllStudents(request, response) {
     let result = 'This is the list of our students\n';
     response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
     return readDatabase('./database.csv')
       .then((courses) => {
         const courseNames = Object.keys(courses);
@@ -15,7 +16,6 @@ export default class StudentsController {
       }).catch(() => {
         response.statusCode = 500;
         response.setHeader('Content-Type', 'text/plain');
-        response.write('This is the list of our students\n');
         response.end('Cannot load the database');
       });
   }
@@ -27,11 +27,12 @@ export default class StudentsController {
     }
     return readDatabase('./database.csv')
       .then((courses) => {
+        response.statusCode = 500;
+        response.setHeader('Content-Type', 'text/plain');
         response.send(`List: ${courses[major].join(', ')}`);
       }).catch(() => {
         response.statusCode = 500;
         response.setHeader('Content-Type', 'text/plain');
-        response.write('This is the list of our students\n');
         response.end('Cannot load the database');
       });
   }
