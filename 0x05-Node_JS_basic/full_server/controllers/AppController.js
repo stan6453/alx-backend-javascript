6205d520af78c3ba165fa539c3e2c9c8c744270f
stanley <stanley6453@gmail.com>
2023-08-22 12:59:13 +0100

    craete a server in node.js and handled reading and manipulating file content

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,6 @@
 export default class AppController {
   static getHomepage(request, response) {
+    response.setHeader('Content-Type', 'text/plain');
     return response.status(200).send('Hello Holberton School!');
   }
 }
