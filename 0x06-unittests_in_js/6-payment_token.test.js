a7bb318b15e933db8d0d82a262024ce927a256df
stanley <stanley6453@gmail.com>
2023-08-23 23:22:43 +0100

    impletemed test for a promisified function

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,7 +2,7 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 const expect = require('chai').expect;
 describe('getPaymentTokenFromAPI', function () {
   it('getPaymentTokenFromAPI should resolve to a valid data when success is true', function (done) {
-    getPaymentTokenFromAPI(true)
+    return getPaymentTokenFromAPI(true)
       .then((data) => {
         expect(data).to.eql({ data: 'Successful response from the API' });
         done();
