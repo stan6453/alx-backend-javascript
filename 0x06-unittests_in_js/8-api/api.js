65d8df5da7e068d14903f2a905982a3fc69bfb67
stanley <stanley6453@gmail.com>
2023-08-24 09:47:16 +0100

    wrote integration test for 8-api

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.get('/', (request, response)=>{
+app.get('/', (request, response) => {
   response.send('Welcome to the payment system');
 });
 
 app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`)
+  console.log(`API available on localhost port ${port}`);
 });
